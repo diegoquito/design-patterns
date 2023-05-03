35493347010e66d6beb9841507905d1ee887dd35
diegoquito <123753456+diegoquito@users.noreply.github.com>
2023-05-03 15:45:49 -0500

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+                  
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+
+            s1.ExampleMethod();
+            s2.ExampleMethod();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton? _instance;
+
+        // Constructor privado para evitar que se instancie la clase desde fuera
+        private Singleton() { }
+
+        // Método para obtener la instancia del Singleton
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        // Método de ejemplo que muestra cómo se utiliza el Singleton
+        public void ExampleMethod()
+        {
+            Console.WriteLine("This is singleton example");
+        }
+    }
+}
