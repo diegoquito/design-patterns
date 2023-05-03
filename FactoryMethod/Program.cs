5d82c9769b80b9adfdf268afbb83371544d12cb4
diegoquito <123753456+diegoquito@users.noreply.github.com>
2023-05-03 15:19:34 -0500

    factory method

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Creator creator;
+            IProduct product;
+
+            creator = new ConcreteCreatorA();
+            product = creator.FactoryMethod();
+            product.Operation();
+
+            creator = new ConcreteCreatorB();
+            product = creator.FactoryMethod();
+            product.Operation();
+        }
+    }
+    
+}
